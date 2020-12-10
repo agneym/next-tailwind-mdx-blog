731f4448cd478827c8d11804af10978933c8b367
Agney <agney.menon@bigbinary.com>
2020-12-10 10:15:48 +0530

    add site header

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -1,7 +1,14 @@
+import { Hash } from "react-feather";
+
 function Header() {
   return (
-    <header className="pt-8 pb-8">
-      <h1 className="text-xl">Mindless Blog</h1>
+    <header className="pt-8 pb-8 flex items-center">
+      <span className="mr-2">
+        <Hash color="#D72638" />
+      </span>
+      <h1 className="text-xl font-medium" style={{ color: "#140F2D" }}>
+        Mindless Blog
+      </h1>
     </header>
   );
 }
