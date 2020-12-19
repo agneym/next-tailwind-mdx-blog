c3194d98e71a46d1bb74ccf229243e15d1f11136
Agney <agney.menon@bigbinary.com>
2020-12-19 21:01:12 +0530

    add remark images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 const withMdxEnhanced = require("next-mdx-enhanced");
 const remarkPrism = require("remark-prism");
+const remarkImages = require("remark-images");
 const remarkBehead = require("remark-behead");
 
 module.exports = withMdxEnhanced({
   layoutPath: "templates",
-  remarkPlugins: [remarkPrism, [remarkBehead, { after: 0, depth: 1 }]],
+  remarkPlugins: [
+    remarkPrism,
+    remarkImages,
+    [remarkBehead, { after: 0, depth: 1 }],
+  ],
   rehypePlugins: [],
   usesSrc: false,
   defaultLayout: true,
