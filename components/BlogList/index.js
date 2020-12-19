12f120b1a83ca144cd0157c5f984c29bfc30ad35
Agney <agney.menon@bigbinary.com>
2020-12-19 20:22:31 +0530

    sort posts by date

--- components/BlogList/index.js
+++ components/BlogList/index.js
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import { useMemo } from "react";
 
 import BlogListItem from "./BlogListItem";
 
 function BlogList({ allPostsData }) {
+  const sortedPosts = useMemo(() => {
+    return allPostsData.sort((a, b) => {
+      return new Date(b.date) - new Date(a.date);
+    });
+  }, [allPostsData]);
   return (
     <ul className="divide-y divide-gray-200">
-      {allPostsData.map((post) => (
+      {sortedPosts.map((post) => (
         <li className="py-12" key={post.__resourcePath}>
           <Link href={`${post.__resourcePath.replace(/\.mdx$/, "")}`}>
             <a>
